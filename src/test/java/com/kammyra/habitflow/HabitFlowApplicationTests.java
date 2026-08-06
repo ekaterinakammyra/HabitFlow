3f776c904211ed5c14d1e1350e27b4667611c4df
Kammyra <ekaterinakammyra@gmail.com>
2026-08-06 19:41:33 +0300

    Initialize Spring Boot project

---
+++ src/test/java/com/kammyra/habitflow/HabitFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kammyra.habitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
